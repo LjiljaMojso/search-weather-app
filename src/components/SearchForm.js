5e0e9d0e601c289f82b5ba576fb636ec6472421c
LjiljaMojso <ljmojso@gmail.com>
2023-02-14 16:53:32 +0100

    remove whitespace from the end of a search term

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const SearchForm = ({ onSubmit }) => {
-  const [term, setTerm] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const [term, setTerm] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
 
-    if (term === "" || !term) return;
-    onSubmit(term);
-    setTerm("");
-  };
-  return (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="search">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search city"
-            onChange={(e) => setTerm(e.target.value)}
-            value={term}
-          />
+        if (term === '' || !term) return;
+        onSubmit(term);
+        setTerm('');
+    };
+    return (
+        <div className="form">
+            <form onSubmit={handleSubmit}>
+                <div className="search">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Search city"
+                        onChange={e => setTerm(e.target.value.trim())}
+                        value={term}
+                    />
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default SearchForm;
